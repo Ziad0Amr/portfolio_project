773b3209dace44c4147467e7e82956c21aed7279
Ziad0Amr <ziad.3amr22@gmail.com>
2024-10-04 01:10:15 +0300

    UbdateLinks

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -17,23 +17,23 @@ const Navbar=function(){
         <>
             <nav className={color? "navbar navbar-expand-lg navbarcontent navbar-bg" : "navbar navbar-expand-lg navbarcontent"}>
                 <div className="container">
-                    <Link className="navbar-brand" to="/">Portfolio</Link>
+                    <Link className="navbar-brand" to="./portfolio_project">Portfolio</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                        <Link className="nav-link active" aria-current="page" to="./portfolio_project">Home</Link>
                         </li>
                         <li className="nav-item">
-                        <Link className="nav-link" to="./project">Project</Link>
+                        <Link className="nav-link" to="/portfolio_project/project">Project</Link>
                         </li>
                         <li className="nav-item">
-                        <Link className="nav-link" to="./about">About</Link>
+                        <Link className="nav-link" to="/portfolio_project/about">About</Link>
                         </li>
                         <li className="nav-item">
-                        <Link className="nav-link" to="./contact">Contact</Link>
+                        <Link className="nav-link" to="/portfolio_project/contact">Contact</Link>
                         </li>
                     </ul>
                     </div>
